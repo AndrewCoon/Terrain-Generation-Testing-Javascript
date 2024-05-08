addbdf35a3dd337be3be7234fbbdccc206c89809
Androo <84476053+Anduwu1@users.noreply.github.com>
2024-05-07 23:17:01 -0600

    added biomedata class

---
+++ biomedata.js
@@ -0,0 +1,39 @@
+class BiomeData {
+
+    height_nodes = []
+    heat_nodes = []
+    humidity_nodes = []
+
+    avg_height = 0
+    avg_heat = 0
+    avg_humidity = 0
+
+    constructor() { }
+
+    static average(vals) {
+        let sum = 0;
+
+        for (let i = 0; i < vals.length; i++) {
+            sum += vals[i];
+        }
+      
+        const average = sum / vals.length;
+        return average;
+    }
+
+    add_node(height, heat, humidity) {
+        this.height_nodes.push(height)
+        this.heat_nodes.push(heat)
+        this.humidity_nodes.push(humidity)
+    }
+
+    compute_averages() {
+        this.avg_height = average(this.height_nodes)
+        this.avg_heat = average(this.heat_nodes)
+        this.avg_humidity = average(this.humidity_nodes)
+    }
+
+    get avg_heat() { return this.avg_heat }
+    get avg_height() { return this.avg_height }
+    get avg_humidity() { return this.avg_humidity }
+}
